34bc67f59eb68d2395312a5d457ddd4a48639a26
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2021-07-26 17:15:15 +0100

    Implement BottomNaviagationView and do some minor refactoring

--- app/src/main/java/com/github/rexfilius/movieviewer/ui/MainActivity.kt
+++ app/src/main/java/com/github/rexfilius/movieviewer/ui/MainActivity.kt
@@ -2,11 +2,37 @@ package com.github.rexfilius.movieviewer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.github.rexfilius.movieviewer.R
+import com.github.rexfilius.movieviewer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Get NavHostFragment and NavController
+        val navHostFrag =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFrag.navController
+
+        // Define AppBarConfiguration
+        val topLevelDestinations = setOf(R.id.movieListFragment, R.id.movieFavoriteFragment)
+        val appBarConfiguration = AppBarConfiguration(topLevelDestinations)
+
+        // Connect Toolbar with NavController
+        binding.mainToolbar.setupWithNavController(navController, appBarConfiguration)
+
+        // Connect BottomNavigationView with NavController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/github/rexfilius/movieviewer/ui/movieFavorite/MovieFavoriteFragment.kt
@@ -0,0 +1,25 @@
+package com.github.rexfilius.movieviewer.ui.movieFavorite
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.github.rexfilius.movieviewer.R
+import com.github.rexfilius.movieviewer.databinding.FragmentMovieFavoriteBinding
+
+class MovieFavoriteFragment : Fragment(R.layout.fragment_movie_favorite) {
+
+    private var movieFavoriteBinding: FragmentMovieFavoriteBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentMovieFavoriteBinding.bind(view)
+        movieFavoriteBinding = binding
+    }
+
+    override fun onDestroyView() {
+        movieFavoriteBinding = null
+        super.onDestroyView()
+    }
+
+}
